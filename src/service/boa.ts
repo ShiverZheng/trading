e9d71bd6b4b196c963a980d66d1219bb03f945e2
shiverzheng <zhengxiwen@funlearnworld.com>
2022-03-22 21:42:15 +0800

    access joinQuant

--- src/service/boa.ts
+++ src/service/boa.ts
@@ -1,6 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { parse } from 'yaml';
+
+const file = fs.readFileSync(path.join(__dirname, '../../config.yml'), 'utf-8');
+const config = parse(file);
+console.log(config);
+
 const boa = require('@pipcook/boa');
 const os = boa.import('os');
 console.log(os.getpid()); // prints the pid from python.
+
+const jqdatasdk = boa.import('jqdatasdk');
+jqdatasdk.auth(config.joinQuant.account, config.joinQuant.password);
 
 import { Provide } from '@midwayjs/decorator';
 
@@ -16,6 +27,8 @@ export class BoaService {
     //   })
     // );
 
+    const res = jqdatasdk.get_price('000001.XSHE', '2017-01-01', '2017-12-31');
+    console.log(res);
     // using bult-in functions
     const { range, len } = boa.builtins();
     const list = range(0, 10); // create a range array

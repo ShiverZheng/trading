b567d0d44b9969ea55f86cf9c8b84a1088f22441
shiverzheng <zhengxiwen@funlearnworld.com>
2022-04-05 14:03:40 +0800

    implement pip uninstall

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-### Install python libraries
+### [Use `bip` to install python libraries](https://github.com/imgcook/boa#install-python-package)
 ```shell
-$ ./node_modules/.bin/bip install <package-name>
+$ ./node_modules/.bin/bip install <LIB_NAME>
 ```
 
-### Use `Gulp` to install python libraries
-- install 
-  > `$ gulp 'pip install' --lib jqdatasdk`
-  
+### Use `Gulp` to manage python libraries
+- install
+  > `$ gulp 'pip install' --lib <LIB_NAME>`
+
+  or specify the library version
+
+  > `$ gulp 'pip install' --lib <LIB_NAME>@<LIB_VERSION>`
+
+- uninstall
+  > `$ gulp 'pip uninstall' --lib <LIB_NAME>`
+
 - sync libraries list
   > `$ gulp 'pip sync'`
 
--- gulpfile.ts
+++ gulpfile.ts
@@ -14,6 +14,11 @@ import { spawn } from 'promisify-child-process';
 const FILE_NAME = 'python-libraries.json';
 const FILE_PATH = path.join(__dirname, FILE_NAME);
 
+enum Method {
+  Install = 'install',
+  Uninstall = 'uninstall',
+}
+
 const isExists = (path: PathLike) => {
   try {
     accessSync(path, constants.F_OK);
@@ -33,16 +38,24 @@ const writeDependencies = (libName: string, version = '') => {
   writeFileSync(FILE_PATH, JSON.stringify(content, null, 4));
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const install = async (libName: string, version = '') => {
-  const { stdout, stderr } = await spawn(
-    path.join(__dirname, '.', 'node_modules', '.bin', 'bip'),
-    ['install', libName],
-    { encoding: 'utf8' }
-  );
-  console.log(stdout);
-  console.log(stderr);
+const removeDependency = (libName: string) => {
+  const content = JSON.parse(getFile(FILE_PATH));
+  delete content[libName];
+  writeFileSync(FILE_PATH, JSON.stringify(content, null, 4));
 };
+
+const exec = async (method: Method, libName: string, version = '') => {
+  return await spawn(
+    path.join(__dirname, '.', 'node_modules', '.bin', 'bip'),
+    [method, version ? `${libName}==${version}` : libName],
+    { encoding: 'utf8', stdio: 'inherit' }
+  );
+};
+
+const install = (libName: string, version = '') =>
+  exec(Method.Install, libName, version);
+
+const uninstall = (libName: string) => exec(Method.Uninstall, libName);
 
 task('pip install', async done => {
   const argv = minimist(process.argv.slice(2));
@@ -50,8 +63,23 @@ task('pip install', async done => {
     const pkg = argv['lib'] as string;
     const libName = pkg.split('@')[0];
     const version = pkg.split('@')[1];
-    await install(libName, version);
+    const { stdout, stderr } = await install(libName, version);
+    console.log(stdout);
+    console.error(stderr);
     writeDependencies(libName, version);
+  }
+  done();
+});
+
+task('pip uninstall', async done => {
+  const argv = minimist(process.argv.slice(2));
+  if ('lib' in argv) {
+    const pkg = argv['lib'] as string;
+    const libName = pkg.split('@')[0];
+    const { stdout, stderr } = await uninstall(libName);
+    console.log(stdout);
+    console.error(stderr);
+    removeDependency(libName);
   }
   done();
 });
